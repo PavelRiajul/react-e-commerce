363b64b8439d77e4309473b4136357de7e0fbc08
Riajul Islam <riajulpislam@gmail.com>
2024-12-07 18:24:12 +0600

    project-setup-announcement-section-complete

---
+++ src/Components/Announcement/Announcement.jsx
@@ -0,0 +1,15 @@
+import "./Announcement.css"
+
+function Announcement() {
+  return (
+    <>
+     <div className="announcement-container">
+        <h1>Free Delivery</h1>
+        <h1>Welcome offer 15% off</h1>
+        <h1>Free Returns</h1>
+     </div>
+    </>
+  )
+}
+
+export default Announcement
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Announcement from "../../Components/Announcement/Announcement"
+
+
+function Home() {
+  return (
+    <div>
+        <Announcement/>
+    </div>
+  )
+}
+
+export default Home
